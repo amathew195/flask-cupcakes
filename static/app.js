07ef0fff8ed00de034618544711a794e14b35be4
Ashley Mathew <amathew195@gmail.com>
2022-10-24 19:27:44 -0500

    completed exercise

--- static/app.js
+++ static/app.js
@@ -8,33 +8,60 @@ const $size = $("#size");
 const $rating = $("#rating");
 const $image = $("#image");
 
+/** This function gets cupcakes from the API and displays it on the HTML */
 async function loadCupcakeList() {
+
   const response = await axios.get("/api/cupcakes");
   const cupcakes = response.data.cupcakes;
   for (const cupcake of cupcakes) {
-    $cupcakesList.append(`<li>${cupcake.flavor}</li>`);
+    let $singleCupcake = $('<div></div>');
+    $singleCupcake.append(`<h5>${cupcake.flavor}</h5>`);
+    $singleCupcake.append(`<img src=${cupcake.image}>`);
+    $singleCupcake.append(`<p>${cupcake.size}</p>`);
+    $cupcakesList.append($singleCupcake);
   }
 }
 
-loadCupcakeList()
-$addButton.on("click",addNewCupcake)
+loadCupcakeList();
 
-async function addNewCupcake(evt){
+$form.on("submit", addNewCupcake); //make this 'submit'
+
+/** This function takes the inputs on the form and creates a new
+ * cupcake and updates the display.
+ */
+
+async function addNewCupcake(evt) {
+
   evt.preventDefault();
   const flavor = $flavor.val();
   const size = $size.val();
   const rating = $rating.val();
   const image = $image.val();
-  // const newFlavor = await updateCupcakeList(flavor,size,rating,image);
-  // $cupcakesList.append(`<li>${newFlavor}</li>`)
-
+  const newCupcake = await updateCupcakeList(flavor, size, rating, image);
+  updateCupcakeDisplay(newCupcake);
 }
-async function updateCupcakeList(flavor,size,rating,image){
-  // const data = {"flavor":flavor,"size":size,"rating":rating,"image":image}
+
+/**This function sends the new cupcake information to the API and
+ * returns the new cupcake instance
+ */
+
+async function updateCupcakeList(flavor, size, rating, image) {
+
   const response = await axios.post(
-      "/api/cupcakes",
-      {"flavor":flavor,"size":size,"rating":rating,"image":image}
-  )
-  console.log(response.data.cupcake.flavor)
-  return response.data.cupcake.flavor
+    "/api/cupcakes",
+    { "flavor": flavor, "size": size, "rating": rating, "image": image }
+  );
+  console.log(response.data.cupcake);
+  return response.data.cupcake;
+}
+
+/** This function updates the display when adding new cupcakes */
+function updateCupcakeDisplay(cupcake) {
+
+
+  let $singleCupcake = $('<div></div>');
+  $singleCupcake.append(`<h5>${cupcake.flavor}</h5>`);
+  $singleCupcake.append(`<img src=${cupcake.image}>`);
+  $singleCupcake.append(`<p>${cupcake.size}</p>`);
+  $cupcakesList.append($singleCupcake);
 }
